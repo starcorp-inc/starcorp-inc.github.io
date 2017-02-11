4172ab9b0998c5f28103aec2e58f7f762aa7109c
starcorp-inc <starcorp@mail.com>
2017-02-11 10:35:33 -0600

    Add files via upload

---
+++ modifier.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ $(".menu-mobile-button").click(function() {
+  $(".menu-mobile").toggle();
+  $(".logo").toggle();
+  $(".footer-mobile").toggle();
+  move();
+ });
+});
+function coming() {
+  alert("Coming soon");
+}
+function showbody() {
+  $(".loading").toggle();
+}
